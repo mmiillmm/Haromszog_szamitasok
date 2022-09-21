2a99d8da160ef0a10fa70a0797e3c4d7950c3407
mmiillmm <kmakai520@gmail.com>
2022-09-21 20:48:07 +0200

    good

--- Haromszog/Program.cs
+++ Haromszog/Program.cs
@@ -12,34 +12,59 @@ namespace Haromszog
         {
             Console.WriteLine("Ez a program kiszámolja egy háromszög kerületét és területét a megadott adatok alapján. A háromszög alapja minden esetben a 'c' oldal. Kérem minden adat megadása után való visszajelzést követően nyomjon egy gombot.");
             Console.WriteLine();
-            Console.Write("Kérem a háromszög 'a' oldalát: ");
-            double ao = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"Az 'a' oldal hosszúsága {ao} cm.");
-            Console.ReadLine();
-            Console.Write("Kérem a háromszög 'b' oldalát: ");
-            double bo = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"A 'b' oldal hosszúsága {bo} cm");
-            Console.ReadLine();
             Console.Write("Kérem a háromszög 'c' oldalát: ");
             double co = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"A 'c' oldal hosszúsága {co} cm");
-            Console.ReadLine();
+            Console.Write("Kérem a háromszög 'b' oldalát: ");
+            double bo = Convert.ToDouble(Console.ReadLine());
+
+            bool firstloop = true;
+
+            while (bo > co)
+            {
+                if (firstloop == false)
+                {
+                    Console.WriteLine("Haver...");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("A megadott adat nem megfelelő. Adj meg újat: ");
+                bo = int.Parse(Console.ReadLine());
+
+                firstloop = false;
+            }
+            Console.Write("Kérem a háromszög 'a' oldalát: ");
+            double ao = Convert.ToDouble(Console.ReadLine());
+
+            bool secondloop = true;
+
+            while (ao > co)
+            {
+                if (secondloop == false)
+                {
+                    Console.WriteLine("Haver...");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("A megadott adat nem megfelelő. Adj meg újat: ");
+                ao = int.Parse(Console.ReadLine());
+
+                secondloop = false;
+            }
+
             Console.Write("Kérem a háromszög magasságát: ");
             double mag = Convert.ToDouble(Console.ReadLine());
-            Console.Write($"A magasság {mag} cm");
-            Console.ReadLine();
 
             char val;
-            Console.SetCursorPosition(20, 14);
+            Console.SetCursorPosition(30, 14);
             Console.WriteLine("1. Kerület");
 
-            Console.SetCursorPosition(20, 15);
+            Console.SetCursorPosition(30, 15);
             Console.WriteLine("2. Terület");
 
-            Console.SetCursorPosition(20, 16);
+            Console.SetCursorPosition(30, 16);
             Console.WriteLine("3. Kilépés");
 
-            Console.SetCursorPosition(35, 18);
+            Console.SetCursorPosition(45, 18);
             Console.WriteLine("Kérem adja meg a választott eljárás számát: ");
             val = Char.Parse(Console.ReadLine());
 
@@ -56,6 +81,11 @@ namespace Haromszog
                     break;
 
                 case '3':
+                    break;
+
+                default:
+                    Console.WriteLine("Nincs ilyen menüpont.");
+                    Console.ReadLine();
                     break;
             }
         }
